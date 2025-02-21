4f1db06c2fd80190cb9f6b8a8d7a0f37a6d78835
1nvisibilia <jiaditao@gmail.com>
2025-02-21 01:09:51 +0000

    save

--- a1q2.c
+++ a1q2.c
@@ -3,7 +3,11 @@
 #include <stdio.h>
     
 #define BUFSIZE 256
-    
+
+int wcWrapAroundQuote(char *newString, char *oldString, size_t size) {
+    snprintf(newString, size, "wc -c < \"%s\"", oldString);
+}
+
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
     // Ensure that the user supplied exactly one command line argument
@@ -14,6 +18,6 @@ int main(int argc, char** argv) {
     char sized_input[BUFSIZE - 10];
     strncpy(sized_input, argv[1], BUFSIZE - 10);
     char cmd[BUFSIZE];
-    sprintf(cmd, "wc -c < \"%s\"", sized_input);
+    wcWrapAroundQuote(cmd, sized_input, BUFSIZE);
     system(cmd);
 }
